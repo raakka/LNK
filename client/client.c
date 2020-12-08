38842e0a031c617915e6bb7babb15d6356a02b02
xvi <jrip03@gmail.com>
2020-12-08 00:10:29 -0500

    valid valid addr

--- client/client.c
+++ client/client.c
@@ -34,7 +34,7 @@ int main() {
 
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(8080);
-	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1:8888");
+	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
 	if (connect(client_socket, (struct sockaddr*) &server_addr,	sizeof(server_addr)) == -1) {
 		perror("Cannot Connect to Server!");	
